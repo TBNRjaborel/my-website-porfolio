5269fa016b4499a2b204f3b35cfaf919696a89a4
Gabriel Magdugo <magdugogabriel@gmail.com>
2025-04-14 21:21:34 +0800

    added about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,27 @@
-<div className ="flex items-center justify-between">
-                    <p>
-                    Previously focused on driving conversion strategies for e-commerce and driving traffic to websites, 
-                    I found myself venturing into the world of software development. With a unique blend of technical expertise 
-                    and marketing insight, I bridge the gap between functionality and user experience
-                    </p>
-                </div>
+import { poppins } from "@/utils/fonts";
+
+
+function AboutPage(){
+    return(
+        <div className ={`${poppins.className} flex flex-col items-center h-screen px-4`}>
+            <h1 className="text-6xl">About Me</h1>
+            <div className="shadow-lg mt-10 p-10 bg-gray-900 rounded-lg max-w-screen-lg">
+                <p className="max-w-screen-lg text-justify">
+                Previously focused on driving conversion strategies for e-commerce and driving traffic to websites, 
+                I found myself venturing into the world of software development. With a unique blend of technical expertise 
+                and marketing insight, I bridge the gap between functionality and user experience.
+                </p>
+                <p className="mt-10 text-justify max-w-screen-lg">Exposed in multiple frameworks and technologies, I am flexible in developing software solutions in web 
+                applications or a mobile application setup. I have experience in various technologies pertaining Full Stack 
+                Software Development (React Native, React, Supabase, Node). Whenever a new tech stack is introduced I believe 
+                I am capable of learning and adjusting to it.
+                </p>
+            </div>
+            
+        </div>
+
+    )
+
+}
+
+export default AboutPage;
